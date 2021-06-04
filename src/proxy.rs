3de89816ec5efaf7b05d536201134c0182095988
magicalne <magicalne@gmail.com>
2021-06-04 15:28:28 +0800

    fix: Cannot init buf with from vec

--- src/proxy.rs
+++ src/proxy.rs
@@ -76,10 +76,8 @@ where
     I: AsyncRead + Unpin,
 {
     fn new(io: I, direction: &'static str) -> Self {
-        let buf = vec![0u8; 81960];
-        let buf = BytesMut::from(&buf[..]);
         Self {
-            buf,
+            buf: BytesMut::with_capacity(81960),
             is_shutdown: false,
             io,
             direction,
